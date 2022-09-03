85e17add011d68f3fc2784e91b4373d11319f996
nosianuri <nurinosia@gmail.com>
2022-09-03 15:06:38 +0600

    service api set

--- index.js
+++ index.js
@@ -1,11 +1,51 @@
 const express =require('express');
-const app = express();
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
 const port = process.env.PORT || 5000;
+
+const app = express();
+
+
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dxm0vva.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        await client.connect();
+        const serviceCollection = client.db('travelMaster').collection('service');
+        
+        app.get('/service', async (req, res) => {
+        const query = {};
+        const cursor = serviceCollection.find(query);
+        const services = await cursor.toArray();
+        res.send(services);
+        });
+
+        app.get('/service/:id', async(req, res) =>{
+            const id = req.params.id;
+            const query={_id: ObjectId(id)};
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+        })
+    }
+    finally{
+
+    }
+}
+
+run().catch(console.dir);
+
 
 app.get('/', (req, res) =>{
     res.send('Running travel master');
 });
 
 app.listen(port, () =>{
-    console.log('Travel master is running');
+    console.log('Travel master is running', port);
 })
